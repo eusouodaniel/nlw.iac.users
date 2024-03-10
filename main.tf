e23b2f28f01cc12ae7ba3149a4caf47a02d93f51
eusouodaniel <danielrodriguesdrs331@gmail.com>
2024-03-10 18:03:12 -0300

    new: create vpc and eks

--- main.tf
+++ main.tf
@@ -48,20 +48,20 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
   }
 }
 
-# module "vpc" {
-#   source         = "./modules/vpc"
-#   prefix         = var.prefix
-#   vpc_cidr_block = var.vpc_cidr_block
-# }
+module "vpc" {
+  source         = "./modules/vpc"
+  prefix         = var.prefix
+  vpc_cidr_block = var.vpc_cidr_block
+}
 
-# module "eks" {
-#   source         = "./modules/eks"
-#   prefix         = var.prefix
-#   vpc_id         = module.vpc.vpc_id
-#   subnet_ids     = module.vpc.subnet_ids
-#   cluster_name   = var.cluster_name
-#   retention_days = var.retention_days
-#   desired_size   = var.desired_size
-#   max_size       = var.max_size
-#   min_size       = var.min_size
-# }
+module "eks" {
+  source         = "./modules/eks"
+  prefix         = var.prefix
+  vpc_id         = module.vpc.vpc_id
+  subnet_ids     = module.vpc.subnet_ids
+  cluster_name   = var.cluster_name
+  retention_days = var.retention_days
+  desired_size   = var.desired_size
+  max_size       = var.max_size
+  min_size       = var.min_size
+}
